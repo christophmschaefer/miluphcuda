0a8282ede907bf627273080b1fe9130594857812
Christoph Burger <c.burger@univie.ac.at>
2020-12-14 17:12:48 +0100

    Deleted old giant collision test case.

--- test_cases/giant_collision_solid/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# set path to CUDA libs:
-export LD_LIBRARY_PATH=/usr/local/cuda/lib64
-
-# miluphCUDA command line:
-nice -19 ./miluphcuda -v -I rk2_adaptive -Q 1e-4 -n 100 -H -t 100.0 -M 10.0 -f impact.0000 -m material.cfg -s -g  > output.txt
