d74506608fe7bddd86809f2914505196efce2eb8
Christoph Schäfer <ch.schaefer@uni-tuebingen.de>
2021-01-13 11:54:39 +0100

    changed path in run script

--- test_cases/giant_collisions/hydro/run.sh
+++ test_cases/giant_collisions/hydro/run.sh
@@ -3,9 +3,9 @@
 # set path to CUDA libs:
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64/:$LD_LIBRARY_PATH
 
-# set path to miluphcuda executable
-MC=../../miluphcuda_14Dec2020/miluphcuda
+# set path to miluphcuda executable [change if necessary]
+MC=../../../miluphcuda
 
 # miluphcuda command line:
-$MC -v -A -f impact.0000 -g -H -I rk2_adaptive -Q 1e-4 -m material.cfg -M 5.0 -n 75 -t 100.0 -s 1>miluphcuda.output 2>miluphcuda.error & disown -h
+$MC -v -A -f impact.0000 -g -H -I rk2_adaptive -Q 1e-4 -m material.cfg -M 5.0 -n 75 -t 100.0 -s 
 
