de6c01e2c38c28d82b0aff1e368372a68d6c7bb4
Christoph Schaefer <cm.schaefer@gmail.com>
2021-01-23 07:05:21 +0100

    (2021-01-23_07-05-01) Automatic documentation generation...

--- docs/search/all_15.js
+++ docs/search/all_15.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['update_5fsink_5fvalues',['UPDATE_SINK_VALUES',['../parameter_8h.html#ab420bf44cd20c313a610ec56f57989e4',1,'UPDATE_SINK_VALUES():&#160;parameter.h'],['../test__cases_2giant__collisions_2hydro_2parameter_8h.html#ab420bf44cd20c313a610ec56f57989e4',1,'UPDATE_SINK_VALUES():&#160;parameter.h'],['../test__cases_2giant__collisions_2solid_2parameter_8h.html#ab420bf44cd20c313a610ec56f57989e4',1,'UPDATE_SINK_VALUES():&#160;parameter.h']]],
+  ['update_5fsink_5fvalues',['UPDATE_SINK_VALUES',['../parameter_8h.html#ab420bf44cd20c313a610ec56f57989e4',1,'UPDATE_SINK_VALUES():&#160;parameter.h'],['../test__cases_2colliding__rings_2parameter_8h.html#ab420bf44cd20c313a610ec56f57989e4',1,'UPDATE_SINK_VALUES():&#160;parameter.h'],['../test__cases_2giant__collisions_2hydro_2parameter_8h.html#ab420bf44cd20c313a610ec56f57989e4',1,'UPDATE_SINK_VALUES():&#160;parameter.h'],['../test__cases_2giant__collisions_2solid_2parameter_8h.html#ab420bf44cd20c313a610ec56f57989e4',1,'UPDATE_SINK_VALUES():&#160;parameter.h']]],
   ['usage',['usage',['../miluph_8cu.html#a1ad940455be56209415d328d78622715',1,'miluph.cu']]],
   ['usage_2emd',['Usage.md',['../_usage_8md.html',1,'']]],
   ['use_5fsignal_5fhandler',['USE_SIGNAL_HANDLER',['../little__helpers_8h.html#af9eed337cbd1f8093966c2b8038ac0d7',1,'little_helpers.h']]]
