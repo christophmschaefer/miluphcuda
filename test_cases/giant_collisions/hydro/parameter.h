a0d2c97603b85af5c3a97430eb99fa69b0ea5dd5
Christoph Burger <c.burger@univie.ac.at>
2021-02-22 12:33:38 +0100

    Updated GI test cases.

--- test_cases/giant_collisions/hydro/parameter.h
+++ test_cases/giant_collisions/hydro/parameter.h
@@ -176,14 +176,6 @@
 //Choose your fighter, beware of units !!
 #define C_GRAVITY C_GRAVITY_SI
 
-// sets a reference density for the ideal gas eos (if used) - 1% of that is used as DENSITY_FLOOR (if activated) of ideal gas
-#define IDEAL_GAS_REFERENCE_RHO 1.0
-
-// if set to 1 and INTEGRATE_DENSITY is 1, the density will not be lower than 1% rho_0 from
-// material.cfg
-// note: see additionally boundaries.cu with functions beforeRHS and afterRHS for boundary conditions
-#define DENSITY_FLOOR 1 // DENSITY FLOOR sets a minimum density for all particles. the floor density is 1% of the lowest density in material.cfg
-
 // set p to 0 if p < 0
 #define REAL_HYDRO 0
 
