d5c079785403a3fbd527b211286a6e80602f029f
Christoph Schaefer <cm.schaefer@gmail.com>
2021-02-23 07:05:29 +0100

    (2021-02-23_07-05-01) Automatic documentation generation...

--- docs/search/defines_12.js
+++ docs/search/defines_12.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['update_5fsink_5fvalues',['UPDATE_SINK_VALUES',['../parameter_8h.html#ab420bf44cd20c313a610ec56f57989e4',1,'UPDATE_SINK_VALUES():&#160;parameter.h'],['../test__cases_2colliding__rings_2parameter_8h.html#ab420bf44cd20c313a610ec56f57989e4',1,'UPDATE_SINK_VALUES():&#160;parameter.h'],['../test__cases_2giant__collisions_2hydro_2parameter_8h.html#ab420bf44cd20c313a610ec56f57989e4',1,'UPDATE_SINK_VALUES():&#160;parameter.h'],['../test__cases_2giant__collisions_2solid_2parameter_8h.html#ab420bf44cd20c313a610ec56f57989e4',1,'UPDATE_SINK_VALUES():&#160;parameter.h']]],
+  ['update_5fsink_5fvalues',['UPDATE_SINK_VALUES',['../examples_2giant__collisions_2hydro_2parameter_8h.html#ab420bf44cd20c313a610ec56f57989e4',1,'UPDATE_SINK_VALUES():&#160;parameter.h'],['../examples_2giant__collisions_2solid_2parameter_8h.html#ab420bf44cd20c313a610ec56f57989e4',1,'UPDATE_SINK_VALUES():&#160;parameter.h'],['../parameter_8h.html#ab420bf44cd20c313a610ec56f57989e4',1,'UPDATE_SINK_VALUES():&#160;parameter.h'],['../test__cases_2colliding__rings_2parameter_8h.html#ab420bf44cd20c313a610ec56f57989e4',1,'UPDATE_SINK_VALUES():&#160;parameter.h']]],
   ['use_5fsignal_5fhandler',['USE_SIGNAL_HANDLER',['../little__helpers_8h.html#af9eed337cbd1f8093966c2b8038ac0d7',1,'little_helpers.h']]]
 ];
--- docs/search/pages_0.js
+++ docs/search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['giant_20collision_20test_20cases_20for_20miluphcuda',['Giant Collision test cases for miluphcuda',['../md__home_schaefer_src_miluphcuda_documentation_miluphcuda_main_test_cases_giant_collisions__usage.html',1,'']]]
+  ['giant_20collision_20examples_20for_20miluphcuda',['Giant Collision examples for miluphcuda',['../md__home_schaefer_src_miluphcuda_documentation_miluphcuda_main_examples_giant_collisions__usage.html',1,'']]]
 ];
