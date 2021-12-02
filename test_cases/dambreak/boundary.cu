c05f9a764cb44afa0865258c3eb5d2a75e8e852e
Christoph Schäfer <cm.schaefer@gmail.com>
2021-12-02 16:34:24 +0100

    /o

--- test_cases/dambreak/boundary.cu
+++ test_cases/dambreak/boundary.cu
@@ -227,7 +227,7 @@ __global__ void BoundaryConditionsAfterRHS(int *interactions)
 #endif
 
         // feel the Earth!
-        if (p.y[i]) < 0.0) {
+        if (p.y[i] < 0.0) {
             p.y[i] = 0.0;
             p.vy[i] = 0.0;
             p.ay[i] = 0.0;
